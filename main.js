ab7cab87debe6d286ec431eb87fc95aa81900678
Jonas Geschke <mail@yonny.de>
2014-07-19 12:02:57 +0200

    main.js updated

--- main.js
+++ main.js
@@ -74,6 +74,12 @@ define(function (require, exports, module) {
             erg = true;
         } else if (word.indexOf("Ü") > -1) {
             erg = true;
+        } else if (word.indexOf("É") > -1) {
+            erg = true;
+        } else if (word.indexOf("È") > -1) {
+            erg = true;
+        } else if (word.indexOf("À") > -1) {
+            erg = true;
         } else {
             erg = false;
         }
@@ -81,7 +87,7 @@ define(function (require, exports, module) {
     }
 
     function clear(text, editor, start, end) {
-        text = text.replace(/§/g, "&sect;").replace(/ä/g, "&auml;").replace(/ü/g, "&uuml;").replace(/ö/g, "&ouml;").replace(/ß/g, "&szlig;").replace(/à/g, "&agrave;").replace(/â/g, "&acirc;").replace(/á/g, "&aacute;").replace(/é/g, "&eacute;").replace(/è/g, "&egrave;").replace(/ê/g, "&ecirc;").replace(/î/g, "&icirc;").replace(/ï/g, "&iuml;").replace(/í/g, "&iacute;").replace(/ô/g, "&ocirc;").replace(/ó/g, "&oacute;").replace(/œ/g, "oe").replace(/ù/g, "&ugrave;").replace(/ü/g, "&uml;").replace(/û/g, "&ucirc;").replace(/«/g, "&laquo;").replace(/»/g, "&raquo;").replace(/’/g, "'").replace(/¿/g, "&iquest;").replace(/¡/g, "&iexcl;").replace(/°/g, "&deg;").replace(/Ä/g, "&Auml;").replace(/Ö/g, "&Ouml;").replace(/Ü/g, "&Uuml;");
+        text = text.replace(/§/g, "&sect;").replace(/ä/g, "&auml;").replace(/ü/g, "&uuml;").replace(/ö/g, "&ouml;").replace(/ß/g, "&szlig;").replace(/à/g, "&agrave;").replace(/â/g, "&acirc;").replace(/á/g, "&aacute;").replace(/é/g, "&eacute;").replace(/è/g, "&egrave;").replace(/ê/g, "&ecirc;").replace(/î/g, "&icirc;").replace(/ï/g, "&iuml;").replace(/í/g, "&iacute;").replace(/ô/g, "&ocirc;").replace(/ó/g, "&oacute;").replace(/œ/g, "oe").replace(/ù/g, "&ugrave;").replace(/ü/g, "&uml;").replace(/û/g, "&ucirc;").replace(/«/g, "&laquo;").replace(/»/g, "&raquo;").replace(/’/g, "'").replace(/¿/g, "&iquest;").replace(/¡/g, "&iexcl;").replace(/°/g, "&deg;").replace(/Ä/g, "&Auml;").replace(/Ö/g, "&Ouml;").replace(/Ü/g, "&Uuml;").replace(/É/g, "&eacute;").replace(/È/g, "&egrave;").replace(/À/g, "&Agrave;");
 
         editor.document.replaceRange(text, start, end);
     }
